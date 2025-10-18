c2b0c24eae40dde5cb052f6916bd64967ad9b666
Ben Villalobos <bevillal@microsoft.com>
2025-10-17 19:32:17 -0700

    Revert "Init"
    This reverts commit 7f459452ac8f2aae7a640be8dbe50ccedcb6810a.

--- extensions/git-base/src/extension.ts
+++ extensions/git-base/src/extension.ts
@@ -7,12 +7,10 @@ import { ExtensionContext } from 'vscode';
 import { registerAPICommands } from './api/api1';
 import { GitBaseExtensionImpl } from './api/extension';
 import { Model } from './model';
-import { registerFoldingProvider } from './foldingProvider';
 
 export function activate(context: ExtensionContext): GitBaseExtensionImpl {
 	const apiImpl = new GitBaseExtensionImpl(new Model());
 	context.subscriptions.push(registerAPICommands(apiImpl));
-	context.subscriptions.push(registerFoldingProvider());
 
 	return apiImpl;
 }
